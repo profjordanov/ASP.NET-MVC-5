1465e154400f14967bd39c7d952fba538f1e2dd1
Jordan Jordanov <d_yugioh@abv.bg>
2017-04-24 09:16:00 +0300

    update

--- MessageBoard/MessageBoard/js/home-index.js
+++ MessageBoard/MessageBoard/js/home-index.js
@@ -1,19 +1,116 @@
 ﻿// home-index.js
-function homeIndexController($scope, $http) {
-  $scope.data = [];
-  $scope.isBusy = true;
+var module = angular.module("homeIndex", []);
 
-  $http.get("/api/v1/topics?includeReplies=true")
-    .then(function (result) {
-      // success
-      angular.copy(result.data, $scope.data);
-    },
-    function () {
-      // error
-      alert("could not load topics");
-    })
-    .then(function () {
-      $scope.isBusy = false;
+module.config(function($routeProvider) {
+    $routeProvider.when("/",
+    {
+        controller: "topicsController",
+        templateUrl: "/templates/topicsView.html"
     });
+
+    $routeProvider.when("/newmessage",
+        {
+            controller: "newTopicController",
+            templateUrl: "/templates/newTopicView.html"
+        }
+    );
+
+    $routeProvider.when("/message/:id",
+    {
+        controller: "singletopicController",
+        templateUrl: "/template/singleTopicView.html"
+    });
+
+    $routeProvider.otherwise({redirectTo: "/"});
+});
+
+module.factory("dataService",
+    function ($http, $q) {
+        var _topics = [];
+        var _isInit = false;
+
+        var _isReady = function() {
+            return _isInit;
+        }
+
+        var _getTopics = function () {
+
+            var deferred = $q.defer();
+
+            $http.get("/api/v1/topics?includeReplies=true")
+                .then(function(result) {
+                        // success
+                    angular.copy(result.data, _topics);
+                        _isInit = true;
+                        deferred.resolve();
+                    },
+                    function() {
+                        // error
+                        deferred.reject();
+
+                    });
+            return deferred.promise;
+        };
+
+        var _addTopic = function(newTopic) {
+            var deferred = $q.defer();
+
+            $http.post("api/v1/topics", newTopic)
+          .then(function (result) {
+              //success
+              var newlyCreatedTopic = result.data;
+                        _topics.splice(0, 0, newlyCreatedTopic);
+                        deferred.resolve(newlyCreatedTopic);
+                    },
+          function () {
+              //error
+              deferred.reject();
+          });
+
+            return deferred.promise;
+        };
+
+        return {
+            topics: _topics,
+            getTopics: _getTopics
+        }
+    });
+
+function topicsController($scope, $http, dataService) {
+  $scope.data = dataService;
+  $scope.isBusy = false;
+
+  if (dataService.isReady() == false) {
+      $scope.isBusy = true;
+        dataService.getTopics()
+      .then(function () {
+          //success
+      },
+      function () {
+          //error
+          alert("could not load topics");
+      })
+  .then(function () {
+      $scope.isBusy = false;
+  });
+    }
+  
 };
+
+function newTopicController($scope, $http, $window) {
+    $scope.newTopic = {};
+
+    $scope.save = function() {
+
+        dataService.addTopic($scope.newTopic)
+            .then(function() {
+                //suc
+                    $window.location = "#/";
+                },
+                function() {
+                    //error
+                    alert("could not save the new topic");
+                });
+    };
+}
 
